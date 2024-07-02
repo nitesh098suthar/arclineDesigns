c3c85e196f5fa66b81e47c84c9291a6ee1fa5e0f
Nitesh Kumar <nitesh098suthar@gmail.com>
2024-07-02 17:25:12 +0530

    design route completed

--- server/routes/designRoutes.js
+++ server/routes/designRoutes.js
@@ -8,18 +8,11 @@ import {
 
 import uploadDesignFiles from "../middlewares/multer.js";
 
-// import singleUpload from "../middlewares/multer.js";
-// import {
-//   authMiddleware,
-//   authorizedAdmin,
-//   authorizedSubscribers,
-// } from "../middlewares/authMiddleware.js";
-
 const Router = express.Router();
 
 Router.route("/design")
   .post(uploadDesignFiles, createDesign)
   .get(getAllDesigns);
-Router.route("/design/:id").put(updateDesign).delete(deleteDesign);
+Router.route("/design/:id").put(uploadDesignFiles,updateDesign).delete(deleteDesign);
 
 export default Router;
