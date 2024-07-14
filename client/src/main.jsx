0d00d9e4cd3ab869b90f2d5c57ebae415d3118aa
nitesh098suhar <nitesh098suthar@gmail.com>
2024-07-14 16:08:56 +0530

    vite change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
